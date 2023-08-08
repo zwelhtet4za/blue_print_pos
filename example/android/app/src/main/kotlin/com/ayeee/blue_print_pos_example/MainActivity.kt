dc661a243635fb3acf9dc141c02dd034882d591d
zwelhtet4za <zwel@za.com.mm>
2023-08-08 10:30:06 +0630

    remove_ios_services

--- example/android/app/src/main/kotlin/com/ayeee/blue_print_pos_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.ayeee.blue_print_pos_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/blue_print_pos.dart
+++ lib/blue_print_pos.dart
@@ -9,15 +9,15 @@ import 'package:blue_print_pos/scanner/blue_scanner.dart';
 import 'package:blue_thermal_printer/blue_thermal_printer.dart' as blue_thermal;
 import 'package:esc_pos_utils_plus/esc_pos_utils.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_blue_plus/flutter_blue_plus.dart' as flutter_blue;
-import 'package:flutter_blue_plus/gen/flutterblueplus.pb.dart' as proto;
+// import 'package:flutter_blue_plus/flutter_blue_plus.dart' as flutter_blue;
+// import 'package:flutter_blue_plus/gen/flutterblueplus.pb.dart' as proto;
 import 'package:image/image.dart' as img;
 import 'package:qr_flutter/qr_flutter.dart';
 
 class BluePrintPos {
   BluePrintPos._() {
     _bluetoothAndroid = blue_thermal.BlueThermalPrinter.instance;
-    _bluetoothIOS = flutter_blue.FlutterBluePlus.instance;
+    // _bluetoothIOS = flutter_blue.FlutterBluePlus.instance;
   }
 
   static BluePrintPos get instance => BluePrintPos._();
@@ -28,10 +28,10 @@ class BluePrintPos {
   blue_thermal.BlueThermalPrinter? _bluetoothAndroid;
 
   /// This field is library to handle in iOS Platform
-  flutter_blue.FlutterBluePlus? _bluetoothIOS;
+  // flutter_blue.FlutterBluePlus? _bluetoothIOS;
 
   /// Bluetooth Device model for iOS
-  flutter_blue.BluetoothDevice? _bluetoothDeviceIOS;
+  // flutter_blue.BluetoothDevice? _bluetoothDeviceIOS;
 
   /// State to get bluetooth is connected
   bool _isConnected = false;
@@ -62,25 +62,26 @@ class BluePrintPos {
             blue_thermal.BluetoothDevice(
                 selectedDevice?.name ?? '', selectedDevice?.address ?? '');
         await _bluetoothAndroid?.connect(bluetoothDeviceAndroid);
-      } else if (Platform.isIOS) {
-        _bluetoothDeviceIOS = flutter_blue.BluetoothDevice.fromProto(
-          proto.BluetoothDevice(
-            name: selectedDevice?.name ?? '',
-            remoteId: selectedDevice?.address ?? '',
-            type: proto.BluetoothDevice_Type.valueOf(selectedDevice?.type ?? 0),
-          ),
-        );
-        final List<flutter_blue.BluetoothDevice> connectedDevices =
-            await _bluetoothIOS?.connectedDevices ??
-                <flutter_blue.BluetoothDevice>[];
-        final int deviceConnectedIndex = connectedDevices
-            .indexWhere((flutter_blue.BluetoothDevice bluetoothDevice) {
-          return bluetoothDevice.id == _bluetoothDeviceIOS?.id;
-        });
-        if (deviceConnectedIndex < 0) {
-          await _bluetoothDeviceIOS?.connect();
-        }
-      }
+      } 
+      // else if (Platform.isIOS) {
+      //   _bluetoothDeviceIOS = flutter_blue.BluetoothDevice.fromProto(
+      //     proto.BluetoothDevice(
+      //       name: selectedDevice?.name ?? '',
+      //       remoteId: selectedDevice?.address ?? '',
+      //       type: proto.BluetoothDevice_Type.valueOf(selectedDevice?.type ?? 0),
+      //     ),
+      //   );
+      //   final List<flutter_blue.BluetoothDevice> connectedDevices =
+      //       await _bluetoothIOS?.connectedDevices ??
+      //           <flutter_blue.BluetoothDevice>[];
+      //   final int deviceConnectedIndex = connectedDevices
+      //       .indexWhere((flutter_blue.BluetoothDevice bluetoothDevice) {
+      //     return bluetoothDevice.id == _bluetoothDeviceIOS?.id;
+      //   });
+      //   if (deviceConnectedIndex < 0) {
+      //     await _bluetoothDeviceIOS?.connect();
+      //   }
+      // }
 
       _isConnected = true;
       selectedDevice?.connected = true;
@@ -102,10 +103,11 @@ class BluePrintPos {
         await _bluetoothAndroid?.disconnect();
       }
       _isConnected = false;
-    } else if (Platform.isIOS) {
-      await _bluetoothDeviceIOS?.disconnect();
-      _isConnected = false;
-    }
+    } 
+    // else if (Platform.isIOS) {
+    //   await _bluetoothDeviceIOS?.disconnect();
+    //   _isConnected = false;
+    // }
 
     return ConnectionStatus.disconnect;
   }
@@ -192,21 +194,22 @@ class BluePrintPos {
       }
       if (Platform.isAndroid) {
         _bluetoothAndroid?.writeBytes(Uint8List.fromList(byteBuffer));
-      } else if (Platform.isIOS) {
-        final List<flutter_blue.BluetoothService> bluetoothServices =
-            await _bluetoothDeviceIOS?.discoverServices() ??
-                <flutter_blue.BluetoothService>[];
-        final flutter_blue.BluetoothService bluetoothService =
-            bluetoothServices.firstWhere(
-          (flutter_blue.BluetoothService service) => service.isPrimary,
-        );
-        final flutter_blue.BluetoothCharacteristic characteristic =
-            bluetoothService.characteristics.firstWhere(
-          (flutter_blue.BluetoothCharacteristic bluetoothCharacteristic) =>
-              bluetoothCharacteristic.properties.write,
-        );
-        await characteristic.write(byteBuffer, withoutResponse: true);
       }
+      //  else if (Platform.isIOS) {
+      //   final List<flutter_blue.BluetoothService> bluetoothServices =
+      //       await _bluetoothDeviceIOS?.discoverServices() ??
+      //           <flutter_blue.BluetoothService>[];
+      //   final flutter_blue.BluetoothService bluetoothService =
+      //       bluetoothServices.firstWhere(
+      //     (flutter_blue.BluetoothService service) => service.isPrimary,
+      //   );
+      //   final flutter_blue.BluetoothCharacteristic characteristic =
+      //       bluetoothService.characteristics.firstWhere(
+      //     (flutter_blue.BluetoothCharacteristic bluetoothCharacteristic) =>
+      //         bluetoothCharacteristic.properties.write,
+      //   );
+      //   await characteristic.write(byteBuffer, withoutResponse: true);
+      // }
     } on Exception catch (error) {
       print('$runtimeType - Error $error');
     }
