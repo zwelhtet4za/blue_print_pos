0989616fbe30fffbd62b5b25e539df08d46d51a2
zwelhtet4za <zwel@za.com.mm>
2023-08-16 15:38:26 +0630

    alignment

--- lib/receipt/receipt_one_col_one_cell.dart
+++ lib/receipt/receipt_one_col_one_cell.dart
@@ -9,7 +9,7 @@ class ReceiptTotalOneColOneCell {
   String get twoColsOneCellHTML => '''
               <tbody>
                   <tr>
-                   <td class="text-center first-table-cell" colspan="3" style="border: 1px solid #000; font-size: 15px;">$value</td>
+                   <td class="number first-table-cell" colspan="3" style="border: 1px solid #000; font-size: 15px;">$value</td>
                    <td class="number custom-table-cell" style="border: 1px solid #000; font-size: 15px;" colspan="1">$totalPrice</td>
                   </tr> 
               </tbody>
